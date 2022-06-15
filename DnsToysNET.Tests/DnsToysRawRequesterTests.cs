cc980e112a22414c91f2fb0307a7e5007d4d84a0
Fatih Doğan <fth.dgn.369@gmail.com>
2022-06-15 22:11:05 +0300

    Added tests for all the supported commands.

--- DnsToysNET.Tests/DnsToysRawRequesterTests.cs
+++ DnsToysNET.Tests/DnsToysRawRequesterTests.cs
@@ -13,7 +13,63 @@ public class DnsToysRawRequesterTests
     [Fact]
     public async Task Raw_GetsHelp()
     {
+        var result = await sut.RequestAsync("help");
+        result.Should().NotBeEmpty();
+    }
+
+    [Fact]
+    public async Task Raw_GetsTime()
+    {
+        var result = await sut.RequestAsync("mumbai.time");
+        result.Should().NotBeEmpty();
+    }
+
+    [Fact]
+    public async Task Raw_GetsWeather()
+    {
+        var result = await sut.RequestAsync("newyork.weather");
+        result.Should().NotBeEmpty();
+    }
+
+    [Fact]
+    public async Task Raw_GetsUnit()
+    {
+        var result = await sut.RequestAsync("42km-mi.unit");
+        result.Should().NotBeEmpty();
+    }
+
+    [Fact]
+    public async Task Raw_GetsFx()
+    {
+        var result = await sut.RequestAsync("100USD-INR.fx");
+        result.Should().NotBeEmpty();
+    }
+
+    [Fact]
+    public async Task Raw_GetsIp()
+    {
         var result = await sut.RequestAsync("ip");
+        result.Should().NotBeEmpty();
+    }
+
+    [Fact]
+    public async Task Raw_GetsWords()
+    {
+        var result = await sut.RequestAsync("987654321.words");
+        result.Should().NotBeEmpty();
+    }
+
+    [Fact]
+    public async Task Raw_GetsPi()
+    {
+        var result = await sut.RequestAsync("pi");
+        result.Should().NotBeEmpty();
+    }
+
+    [Fact]
+    public async Task Raw_GetsBase()
+    {
+        var result = await sut.RequestAsync("100dec-hex.base");
         result.Should().NotBeEmpty();
     }
 }
